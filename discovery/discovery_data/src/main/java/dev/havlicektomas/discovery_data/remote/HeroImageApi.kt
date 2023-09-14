191a5e162524262191bc22ab09ef42c6cc980bf4
Tomas Havlicek <havlicek.tomas@gmail.com>
2023-09-14 18:20:55 +0200

    Updated fake api responses

--- discovery/discovery_data/src/main/java/dev/havlicektomas/discovery_data/remote/HeroImageApi.kt
+++ discovery/discovery_data/src/main/java/dev/havlicektomas/discovery_data/remote/HeroImageApi.kt
@@ -23,15 +23,15 @@ suspend fun fakeHeroImageApiResponse():Response<List<HeroImageDto>> {
         listOf(
             HeroImageDto(
                 id = "1",
-                imageUrl = "https://firebasestorage.googleapis.com/v0/b/ecommerce-multimodule.appspot.com/o/cabbage.webp?alt=media&token=de5c45db-a017-43b1-aae8-9dd0bf9f1085"
+                imageUrl = "https://firebasestorage.googleapis.com/v0/b/ecommerce-multimodule.appspot.com/o/demo%2Ffruits%2Fblueberry.webp?alt=media&token=e8fa83ee-c731-42fb-8c96-2684db784b1b"
             ),
             HeroImageDto(
                 id = "2",
-                imageUrl = "https://firebasestorage.googleapis.com/v0/b/ecommerce-multimodule.appspot.com/o/cabbage.webp?alt=media&token=de5c45db-a017-43b1-aae8-9dd0bf9f1085"
+                imageUrl = "https://firebasestorage.googleapis.com/v0/b/ecommerce-multimodule.appspot.com/o/demo%2Ffruits%2Fraspberry.webp?alt=media&token=469ee627-6e3c-4445-9fc5-20a21d5c6a38"
             ),
             HeroImageDto(
                 id = "3",
-                imageUrl = "https://firebasestorage.googleapis.com/v0/b/ecommerce-multimodule.appspot.com/o/cabbage.webp?alt=media&token=de5c45db-a017-43b1-aae8-9dd0bf9f1085"
+                imageUrl = "https://firebasestorage.googleapis.com/v0/b/ecommerce-multimodule.appspot.com/o/demo%2Fveggies%2Fradishes.webp?alt=media&token=c1c11568-a449-4cbc-b4f5-d3d4ff3a311b"
             )
         )
     )
--- discovery/discovery_data/src/main/java/dev/havlicektomas/discovery_data/remote/ProductApi.kt
+++ discovery/discovery_data/src/main/java/dev/havlicektomas/discovery_data/remote/ProductApi.kt
@@ -52,11 +52,7 @@ private fun fakeHomeProductsApiResponse() = Response.success(
     ProductSearchDto(
         page = 1,
         pageSize = 9,
-        products = listOf(
-            fakeFeaturedProductDto(), fakeFeaturedProductDto(), fakeFeaturedProductDto(),
-            fakeOnsaleProductDto(), fakeOnsaleProductDto(), fakeOnsaleProductDto(),
-            fakeForyouProductDto(), fakeForyouProductDto(), fakeForyouProductDto(),
-        )
+        products = fakeFeaturedProductDtos() + fakeOnsaleProductDtos() + fakeForyouProductDtos()
     )
 )
 
@@ -64,9 +60,7 @@ private fun fakeFavouriteProductsApiResponse() = Response.success(
     ProductSearchDto(
         page = 1,
         pageSize = 9,
-        products = listOf(
-            fakeFeaturedProductDto(), fakeFeaturedProductDto(), fakeFeaturedProductDto()
-        )
+        products = fakeFeaturedProductDtos()
     )
 )
 
@@ -74,43 +68,105 @@ private fun fakeSearchProductsApiResponse() = Response.success(
     ProductSearchDto(
         page = 1,
         pageSize = 9,
-        products = listOf(
-            fakeFeaturedProductDto(), fakeFeaturedProductDto(), fakeFeaturedProductDto(),
-            fakeOnsaleProductDto(), fakeOnsaleProductDto(), fakeOnsaleProductDto(),
-            fakeForyouProductDto(), fakeForyouProductDto(), fakeForyouProductDto(),
-        )
+        products = fakeFeaturedProductDtos() + fakeOnsaleProductDtos() + fakeForyouProductDtos()
     )
 )
 
-private fun fakeFeaturedProductDto() = ProductDto(
-    id = "123",
-    name = "Product 1",
-    brand = "SomeBrand",
-    description = "Some description goes here",
-    price = 9.99,
-    category = "Category1",
-    tag = "featured",
-    imageUrl = "https://firebasestorage.googleapis.com/v0/b/ecommerce-multimodule.appspot.com/o/cabbage.webp?alt=media&token=de5c45db-a017-43b1-aae8-9dd0bf9f1085"
+private fun fakeFeaturedProductDtos() = listOf(
+    ProductDto(
+        id = "123",
+        name = "Apples",
+        brand = "SomeBrand",
+        description = "Some description goes here",
+        price = 9.99,
+        category = "fruits",
+        tag = "featured",
+        imageUrl = "https://firebasestorage.googleapis.com/v0/b/ecommerce-multimodule.appspot.com/o/demo%2Ffruits%2Fapple.webp?alt=media&token=3030a598-e083-40c1-b372-ab06d151d67c"
+    ),
+    ProductDto(
+        id = "123",
+        name = "Bananas",
+        brand = "SomeBrand",
+        description = "Some description goes here",
+        price = 9.99,
+        category = "fruits",
+        tag = "featured",
+        imageUrl = "https://firebasestorage.googleapis.com/v0/b/ecommerce-multimodule.appspot.com/o/demo%2Ffruits%2Fbanana.webp?alt=media&token=5ffa4dc6-7beb-42e6-a69d-362edc55c37a"
+    ),
+    ProductDto(
+        id = "123",
+        name = "Blueberries",
+        brand = "SomeBrand",
+        description = "Some description goes here",
+        price = 9.99,
+        category = "fruits",
+        tag = "featured",
+        imageUrl = "https://firebasestorage.googleapis.com/v0/b/ecommerce-multimodule.appspot.com/o/demo%2Ffruits%2Fblueberry.webp?alt=media&token=e8fa83ee-c731-42fb-8c96-2684db784b1b"
+    )
 )
 
-private fun fakeOnsaleProductDto() = ProductDto(
-    id = "123",
-    name = "Product 1",
-    brand = "SomeBrand",
-    description = "Some description goes here",
-    price = 9.99,
-    category = "Category1",
-    tag = "onsale",
-    imageUrl = "https://firebasestorage.googleapis.com/v0/b/ecommerce-multimodule.appspot.com/o/cabbage.webp?alt=media&token=de5c45db-a017-43b1-aae8-9dd0bf9f1085"
+private fun fakeOnsaleProductDtos() = listOf(
+    ProductDto(
+        id = "123",
+        name = "Raddish",
+        brand = "SomeBrand",
+        description = "Some description goes here",
+        price = 9.99,
+        category = "fruits",
+        tag = "onsale",
+        imageUrl = "https://firebasestorage.googleapis.com/v0/b/ecommerce-multimodule.appspot.com/o/demo%2Fveggies%2Fradishes.webp?alt=media&token=c1c11568-a449-4cbc-b4f5-d3d4ff3a311b"
+    ),
+    ProductDto(
+        id = "123",
+        name = "Cabbage",
+        brand = "SomeBrand",
+        description = "Some description goes here",
+        price = 9.99,
+        category = "fruits",
+        tag = "onsale",
+        imageUrl = "https://firebasestorage.googleapis.com/v0/b/ecommerce-multimodule.appspot.com/o/demo%2Fveggies%2Fcabbage.webp?alt=media&token=ba30f0c6-15bb-43ce-8d8e-df4eb2edcb90"
+    ),
+    ProductDto(
+        id = "123",
+        name = "Carrot",
+        brand = "SomeBrand",
+        description = "Some description goes here",
+        price = 9.99,
+        category = "fruits",
+        tag = "onsale",
+        imageUrl = "https://firebasestorage.googleapis.com/v0/b/ecommerce-multimodule.appspot.com/o/demo%2Fveggies%2Fcarrots.webp?alt=media&token=b3eec187-0277-4a91-9ed3-88ef16e4c718"
+    )
 )
 
-private fun fakeForyouProductDto() = ProductDto(
-    id = "123",
-    name = "Product 1",
-    brand = "SomeBrand",
-    description = "Some description goes here",
-    price = 9.99,
-    category = "Category1",
-    tag = "foryou",
-    imageUrl = "https://firebasestorage.googleapis.com/v0/b/ecommerce-multimodule.appspot.com/o/cabbage.webp?alt=media&token=de5c45db-a017-43b1-aae8-9dd0bf9f1085"
+private fun fakeForyouProductDtos() = listOf(
+    ProductDto(
+        id = "123",
+        name = "Pinapple",
+        brand = "SomeBrand",
+        description = "Some description goes here",
+        price = 9.99,
+        category = "fruits",
+        tag = "foryou",
+        imageUrl = "https://firebasestorage.googleapis.com/v0/b/ecommerce-multimodule.appspot.com/o/demo%2Ffruits%2Fpinapple.webp?alt=media&token=13b4888e-d8ac-405e-b328-35aec118ec90"
+    ),
+    ProductDto(
+        id = "123",
+        name = "Strawberry",
+        brand = "SomeBrand",
+        description = "Some description goes here",
+        price = 9.99,
+        category = "fruits",
+        tag = "foryou",
+        imageUrl = "https://firebasestorage.googleapis.com/v0/b/ecommerce-multimodule.appspot.com/o/demo%2Ffruits%2Fstrawberry.webp?alt=media&token=539ff617-f01f-41eb-b66a-51eb0392f8c1"
+    ),
+    ProductDto(
+        id = "123",
+        name = "Peaches",
+        brand = "SomeBrand",
+        description = "Some description goes here",
+        price = 9.99,
+        category = "fruits",
+        tag = "foryou",
+        imageUrl = "https://firebasestorage.googleapis.com/v0/b/ecommerce-multimodule.appspot.com/o/demo%2Ffruits%2Fpeaches.webp?alt=media&token=7d24f30c-b09e-4b47-b761-d32766411256"
+    )
 )
